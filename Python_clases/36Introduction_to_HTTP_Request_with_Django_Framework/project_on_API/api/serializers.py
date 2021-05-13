51cbba54d49d76d3601385aa0ea4cb6d48f4bce3
Rossi Otali <rotali77@gmail.com>
2021-05-13 18:25:19 +0100

    Added again

---
+++ Python_clases/36Introduction_to_HTTP_Request_with_Django_Framework/project_on_API/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Student
+
+class studentserializers(serializers.ModelSerializer):
+
+    # name = serializers.CharField(max_length=255)
+    # email = serializers.EmailField()
+    # reg_no= serializers.CharField()
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+  
--- Python_clases/36Introduction_to_HTTP_Request_with_Django_Framework/project_on_API/api/views.py
+++ Python_clases/36Introduction_to_HTTP_Request_with_Django_Framework/project_on_API/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response 
 
+from .models import Student
+from .serializers import studentserializers
 # Create your views here.
+
+
+#making a post request
+class StudentViews(generics.CreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class= studentserializers
+
+    # def Create(self, request, *args, **kwargs):
+    #     serializer = self.serializer_class(data=request.data)
+
+    # def perform_create(self,serializer):
+    #     return super().perform_create(serializer)
+
+# @api_view(['POST'])
+# def create_student(request):
+#     # student=Student.objects.all()
+#     serializer=studentserializers(data=request.data)
+
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+class GetStudentViews(generics.ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class= studentserializers
+
+
+class updatestudentviews(generics.UpdateAPIView):
+    queryset=Student.objects.all()
+    serializer_class= studentserializers
+    
+class DeleteStudentViews(generics.DestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class= studentserializers
+    
